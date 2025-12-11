3f421d929a6b75b0a509f245d050ab7a52408422
kiranaswamy <kiranswamygowda72@gmail.com>
2025-12-11 18:40:01 +0530

    adding transaction

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,34 +1,45 @@
 const Expense = require("../module/expenseModule");
 const User = require('../module/userModule');
-const { fn, col } = require("sequelize");
+const { fn, col} = require("sequelize");
+const sequelize = require('../utils/db-connections')
 
 
 const addExpense = async (req, res) => {
   console.log(req)
+   const t =await sequelize.transaction();
   try {
-    const { amount, description, category } = req.body;
+    const { amount, description, category ,updateTotal = false } = req.body;
     const userId = req.userId; 
     console.log("User ID:", userId);
 
     if (!amount || !description) {
+      await t.rollback()
       return res.status(400).json({ error: "Amount and Description required" });
     }
-
-     await User.increment("totalExpense", {
-      by: amount,
-      where: { id: userId }
-    });
 
     const expense = await Expense.create({
       amount,
       description,
       category,
       UserId: userId,
-    });
+      
+    },
+    {transaction:t});
 
+    if(updateTotal){
+    await User.increment("totalExpense", {
+      by: amount,
+      where: { id: userId },
+      transaction:t
+    });
+  }
+
+    await t.commit();
     return res.status(201).json(expense);
+    
   } catch (error) {
     console.error(error);
+    await t.rollback();
     res.status(500).json({ error: "Failed to add expense" });
   }
 };
@@ -46,18 +57,26 @@ const getExpenses = async (req, res) => {
 
 
 const deleteExpense = async (req, res) => {
+   const t = await sequelize.transaction();
   try {
     const id = req.params.id;
+    const exp = await Expense.findByPk(id);
+      if (!exp) {
+      return res.status(404).json({ error: "Expense not found" });
+    }
 
  await User.decrement("totalExpense", {
       by: exp.amount,
-      where: { id: exp.UserId }
+      where: { id: exp.UserId },
+      transaction:t
     });
 
-    await Expense.destroy({ where: { id } });
+    await Expense.destroy({ where: { id },transaction:t });
+    await t.commit();
     res.json({ message: "Expense deleted" });
   } catch (err) {
     console.error(err);
+    await t.rollback();
     res.status(500).json({ error: "Failed to delete expense" });
   }
 };
